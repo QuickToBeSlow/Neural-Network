6edcca78de874c1c17d172c64769d6d03232d4d7
Blake <35315543+QuickToBeSlow@users.noreply.github.com>
2019-10-30 14:13:20 -0500

    Forgot to add in the other functions.

--- source.js
+++ source.js
@@ -326,3 +326,35 @@ function quickSort(items, left, right) {
 
     return items;
 }
+
+function partition(items, left, right) {
+
+    var pivot   = items[Math.floor((right + left) / 2)],
+        i       = left,
+        j       = right;
+
+
+    while (i <= j) {
+
+        while (items[i] < pivot) {
+            i++;
+        }
+
+        while (items[j] > pivot) {
+            j--;
+        }
+
+        if (i <= j) {
+            swap(items, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    return i;
+}
+function swap(items, firstIndex, secondIndex){
+    var temp = items[firstIndex];
+    items[firstIndex] = items[secondIndex];
+    items[secondIndex] = temp;
+}
